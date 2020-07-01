a0e29162f4b85b4649673ff364a08ffacecba41d
sasha370 <kng.sasha@mail.ru>
2020-07-01 18:47:18 +0300

    add Scaffold Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # задаем путь в виде Контроллер\ метод в контроллере, который будет обрабатывать запрос
   # в даннои случае базовый контроллер, у которого должен быть метод HELLO
