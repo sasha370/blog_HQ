f124af18b354bd5c41c4ce799ba5d0ba2f00ba03
sasha370 <kng.sasha@mail.ru>
2020-07-04 08:09:03 +0300

    make Messages View

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+# Данные строчки отключают изменение слассов тегов при показе ошибок
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
