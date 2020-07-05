d91cc285e8df511df8d756e4eee1689c2582f4a2
sasha370 <kng.sasha@mail.ru>
2020-07-05 13:51:14 +0300

    add Edit\Update action for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,32 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Изменения созранены' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
   def create
     @user = User.new(user_params)
 
