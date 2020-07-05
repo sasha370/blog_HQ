354cd999cb848eeb948bee1eb9d274a44806d2fd
sasha370 <kng.sasha@mail.ru>
2020-07-05 13:31:49 +0300

    add Registration cars User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,28 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
 
+    respond_to do |format|
+      # Если сохранение удачно, то перенаправляем на шаблон show, в котором есть место для notice
+      if @user.save
+        format.html { redirect_to articles_path, notice: "Пользователь #{@user.username} успешно создан" }
+        format.json { render :show, status: :created, location: @user }
+      else
+        # если сохранение неудачно, то открываем шаблон NEW ? который подсасывает форму, в которой есть Errors
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 
 end
