f0600fc1716f9cd023c451e45d74567330a47444
sasha370 <kng.sasha@mail.ru>
2020-07-05 16:56:49 +0300

    add LogIn\Out link in navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,21 @@ class SessionsController < ApplicationController
 
   def create
 
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Вы успешно вошли"
+      redirect_to user
+    else
+      flash.now[:alert] = "Вы что-то ввели неправильно"
+      render 'new'
+    end
   end
 
 
   def destroy
-    
+    session[:user_id] = nil
+    flash[:notice] = "Вы вышли"
+    redirect_to root_path
   end
 end
