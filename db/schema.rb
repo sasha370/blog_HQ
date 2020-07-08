ac05a6e935ddb5061b3f38f5b1e116a60a5c6915
sasha370 <kng.sasha@mail.ru>
2020-07-08 14:26:34 +0300

    Add ArticleCategories model many-to-many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_165024) do
+ActiveRecord::Schema.define(version: 2020_07_08_111728) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
