86333dbc9882286bc4a520e08a441279d1e14813
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:50:17 +0300

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
 
-  # Метод Home имеет свой шаблон отображения Pages/Home
   def home
     redirect_to articles_path if logged_in?
   end
 
-  # Метод About имеет свой шаблон отображения Pages/about
   def about
   end
 end
