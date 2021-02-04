8abda389868ab2a1e758f2ac82263ae9e6f1feb0
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:55:56 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # задаем путь в виде Контроллер\ метод в контроллере, который будет обрабатывать запрос
-  # в даннои случае базовый контроллер, у которого должен быть метод HELLO
-  root 'pages#home'
+   root 'pages#home'
 
-  # Все запросы GET на страницу About отправляем в контроллер pages и обрабатываем методом about
   get 'about', to: 'pages#about'
-  resources :articles # прописываем все возможные пути для Articles по CRUD
+  resources :articles
 
-  # переадресуем запрос на создание User на страницу с названием signup
   get 'signup', to: 'users#new'
 
   resources :users, except: [:new]
