79f06d6e074ac516be453e64384b977cc78ef2a8
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:48:52 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    # Если у нас уже есть Текущий пользователь, то мы просто возвращаем его,
-    # если его нет, то создаем нового
-    # "nj нужно, чтобы каждый раз не запрашивать БД
     @carrent_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-
   def logged_in?
-    # превращает переиенную в Булевое значение
     !!current_user
   end
-
 
   def require_user
     if !logged_in?
@@ -22,5 +16,4 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
-
 end
